d5d48c495357468a94a2ab0a88e50ff9698e0267
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-06-17 13:44:56 -0400

    Starting on API options

---
+++ sketches/api-crawl/main.go
@@ -0,0 +1,11 @@
+// This program crawls the LOC.gov API for a single collection and outputs basic
+//  metadata about each item.
+package main
+
+import "log"
+
+const a = "this"
+
+func main() {
+	log.Println("Hello world ")
+}
