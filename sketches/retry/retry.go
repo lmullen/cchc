e3ae950ad35d7c52afb8df20be4c50fedf33897b
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-07-23 13:27:16 -0400

    Change parameters

--- sketches/retry/retry.go
+++ sketches/retry/retry.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/hashicorp/go-retryablehttp"
@@ -13,8 +14,8 @@ func main() {
 	// Set up a client to use for all HTTP requests. It will automatically retry.
 	rc := retryablehttp.NewClient()
 	rc.RetryWaitMin = 2 * time.Second
-	rc.RetryWaitMax = 2 * time.Minute
-	rc.RetryMax = 6
+	rc.RetryWaitMax = 16 * time.Second
+	rc.RetryMax = 3
 	rc.HTTPClient.Timeout = 60 * time.Second
 	// rc.Logger = nil
 	rc.RequestLogHook = func(_ retryablehttp.Logger, req *http.Request, attempt int) {
@@ -25,9 +26,29 @@ func main() {
 	}
 	client := rc.StandardClient()
 
-	res, err := client.Get("https://httpstat.us/500")
-	if err != nil {
-		log.Error("Error: ", err)
-	}
-	log.Info("Result: ", res)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// Test 5xx errors
+	go func() {
+		defer wg.Done()
+		res, err := client.Get("https://httpstat.us/500")
+		if err != nil {
+			log.Error("Error: ", err)
+		}
+		log.Info("Result: ", res)
+	}()
+
+	// Test 429 errors
+	go func() {
+		defer wg.Done()
+		res, err := client.Get("https://httpstat.us/429")
+		if err != nil {
+			log.Error("Error: ", err)
+		}
+		log.Info("Result: ", res)
+	}()
+
+	wg.Wait()
+
 }
