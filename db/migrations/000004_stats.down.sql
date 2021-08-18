c13af5003f6a4937aee1201fea00169c56acd118
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-08-17 21:28:59 -0400

    Migration for db stats

---
+++ db/migrations/000004_stats.down.sql
@@ -0,0 +1,8 @@
+ALTER VIEW stats.count_mimetypes SET SCHEMA public;
+
+DROP VIEW IF EXISTS stats.items_per_collection;
+
+DROP VIEW IF EXISTS stats.items_status;
+
+DROP SCHEMA IF EXISTS stats;
+
---
+++ db/migrations/000004_stats.up.sql
@@ -0,0 +1,44 @@
+CREATE SCHEMA IF NOT EXISTS stats;
+
+ALTER VIEW count_mimetypes SET SCHEMA stats;
+
+CREATE VIEW items_per_collection AS
+SELECT
+  collections.title,
+  COUNT(items_in_collections.collection_id) AS n_items,
+  collections.id
+FROM
+  collections
+  LEFT JOIN items_in_collections ON collections.id = items_in_collections.collection_id
+GROUP BY
+  collections.id
+ORDER BY
+  n_items DESC;
+
+ALTER VIEW items_per_collection SET SCHEMA stats;
+
+CREATE VIEW items_status AS (
+  SELECT
+    'total items' AS type,
+    COUNT(*) AS n
+  FROM
+    items)
+UNION (
+  SELECT
+    'fetched items' AS type,
+    COUNT(*) AS n
+  FROM
+    items
+  WHERE
+    api IS NOT NULL)
+UNION (
+  SELECT
+    'unfetched items' AS type,
+    COUNT(*) AS n
+  FROM
+    items
+  WHERE
+    api IS NULL);
+
+ALTER VIEW items_status SET SCHEMA stats;
+
