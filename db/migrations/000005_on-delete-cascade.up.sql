9483ed2e85b2a0a4dba5e11f4508c21bfd370b42
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-08-18 10:37:35 -0400

    Cascade deletes from items

---
+++ db/migrations/000005_on-delete-cascade.up.sql
@@ -0,0 +1,12 @@
+ALTER TABLE files
+  DROP CONSTRAINT files_item_id_fkey,
+  ADD CONSTRAINT files_item_id_fkey FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE;
+
+ALTER TABLE items_in_collections
+  DROP CONSTRAINT items_in_collections_item_id_fkey,
+  ADD CONSTRAINT items_in_collections_item_id_fkey FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE;
+
+ALTER TABLE resources
+  DROP CONSTRAINT resources_item_id_fkey,
+  ADD CONSTRAINT resources_item_id_fkey FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE;
+
