1700f3e61b2047b89b78ae2ffbd55ffa2e3eeab5
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-10-26 09:59:41 -0400

    DB migration for results

--- db/migrations/000009_quotations.down.sql
+++ db/migrations/000009_quotations.down.sql
@@ -1,1 +1,4 @@
+DROP TABLE IF EXISTS results.biblical_quotations;
+
 DROP SCHEMA IF EXISTS results;
+
--- db/migrations/000009_quotations.up.sql
+++ db/migrations/000009_quotations.up.sql
@@ -1,6 +1,10 @@
 CREATE SCHEMA IF NOT EXISTS results;
 
 CREATE TABLE IF NOT EXISTS results.biblical_quotations (
-  job_id item_id,
-  reference_id verse_id,
-  probability
+  job_id uuid REFERENCES jobs.fulltext_predict (id) NOT NULL,
+  item_id text REFERENCES items (id) NOT NULL,
+  reference_id text NOT NULL,
+  verse_id text NOT NULL,
+  probability real NOT NULL
+);
+
