78fe902c2b6a88b2707434788ebf484b201f8680
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-11-04 19:52:46 -0400

    Refactor qftext to use common connectiosn

--- qftext/app.go
+++ qftext/app.go
@@ -14,7 +14,6 @@ import (
 	"github.com/lmullen/cchc/common/messages"
 	"github.com/microcosm-cc/bluemonday"
 	log "github.com/sirupsen/logrus"
-	"github.com/streadway/amqp"
 )
 
 // App holds resources and config
@@ -28,7 +27,7 @@ type App struct {
 
 // Init connects to all the app's resources and sets the config
 func (app *App) Init() error {
-	log.Info("Creating jobs for the prediction model from items with full text")
+	log.Info("Starting qftext")
 
 	// Set the logging level
 	ll, exists := os.LookupEnv("CCHC_LOGLEVEL")
@@ -48,11 +47,15 @@ func (app *App) Init() error {
 	}
 
 	log.Info("Connecting to the database")
+	dbstr, exists := os.LookupEnv("CCHC_DBSTR")
+	if !exists {
+		return errors.New("Database connection string not set: use CCHC_DBSTR environment variable")
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
-	db, err := db.Connect(ctx)
+	db, err := db.Connect(ctx, dbstr)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error connecting to database: %w", err)
 	}
 	app.DB = db
 	log.Info("Successfully connected to the database")
@@ -67,46 +70,15 @@ func (app *App) Init() error {
 
 	mqstr, exists := os.LookupEnv("CCHC_MQSTR")
 	if !exists {
-		return errors.New("Message broker connection string not set; use CCHC_MQSTR environment variable")
+		return errors.New("Message broker connection string not set: use CCHC_MQSTR environment variable")
 	}
 	log.Info("Attempting to connect to the message broker")
-	rabbit, err := amqp.Dial(mqstr)
+
+	rabbit, err := messages.NewRabbitMQ(ctx, mqstr, "fulltext-predict", 8)
 	if err != nil {
 		return fmt.Errorf("Error connecting to message broker: %w", err)
 	}
-
-	ch, err := rabbit.Channel()
-	if err != nil {
-		return fmt.Errorf("Failed to open a channel on message broker: %w", err)
-	}
-	err = ch.Qos(8, 0, true)
-	if err != nil {
-		return fmt.Errorf("Failed to set prefetch on the message broker: %w", err)
-	}
-	dle, dlq, dlk := "failed-fulltext", "dead-letter-queue", "dead-letter-key"
-	err = ch.ExchangeDeclare(dle, "fanout", true, false, false, false, amqp.Table{})
-	if err != nil {
-		return fmt.Errorf("Failed to declare the dead letter exchange: %w", err)
-	}
-	_, err = ch.QueueDeclare(dlq, true, false, false, false, amqp.Table{})
-	if err != nil {
-		return fmt.Errorf("Failed to declare the dead letter exchange: %w", err)
-	}
-	err = ch.QueueBind(dlq, dlk, dle, false, amqp.Table{})
-	if err != nil {
-		return fmt.Errorf("Failed to bind dead letter queue and exchange: %w", err)
-	}
-	q, err := ch.QueueDeclare("fulltext-predict", true, false, false, false,
-		amqp.Table{
-			// "x-max-length":           10000000,
-			"x-queue-mode":           "lazy",
-			"x-dead-letter-exchange": dle,
-		})
-	if err != nil {
-		return fmt.Errorf("Failed to declare a queue: %w", err)
-	}
-
-	app.MR = messages.NewMessageRepo(ch, &q, nil)
+	app.MR = rabbit
 	log.Info("Successfully connected to the message broker")
 
 	return nil
