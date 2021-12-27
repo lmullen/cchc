80b5cfe9a3a89b40d40181b0768563d3704f9ddb
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-12-27 18:55:56 -0500

    qftext will eventually be removed

--- qftext/main.go
+++ qftext/main.go
@@ -1,55 +1,55 @@
 package main
 
-import (
-	"context"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
+// import (
+// 	"context"
+// 	"os"
+// 	"os/signal"
+// 	"sync"
+// 	"syscall"
 
-	log "github.com/sirupsen/logrus"
-)
+// 	log "github.com/sirupsen/logrus"
+// )
 
-var app = &App{}
+// var app = &App{}
 
-func main() {
-	// Check for interrupt signals and stop gracefully
-	ctx, cancel := context.WithCancel(context.Background())
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	defer func() {
-		signal.Stop(quit)
-		cancel()
-	}()
-	go func() {
-		select {
-		case <-quit:
-			log.Info("Received signal to quit the process ...")
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
+// func main() {
+// 	// Check for interrupt signals and stop gracefully
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	quit := make(chan os.Signal, 1)
+// 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+// 	defer func() {
+// 		signal.Stop(quit)
+// 		cancel()
+// 	}()
+// 	go func() {
+// 		select {
+// 		case <-quit:
+// 			log.Info("Received signal to quit the process ...")
+// 			cancel()
+// 		case <-ctx.Done():
+// 		}
+// 	}()
 
-	err := app.Init(ctx)
-	if err != nil {
-		log.WithError(err).Fatal("Error initializing application")
-	}
-	defer app.Shutdown()
+// 	err := app.Init(ctx)
+// 	if err != nil {
+// 		log.WithError(err).Fatal("Error initializing application")
+// 	}
+// 	defer app.Shutdown()
 
-	wg := sync.WaitGroup{}
+// 	wg := sync.WaitGroup{}
 
-	wg.Add(1)
-	go func() {
-		enqueueForQuotations(ctx)
-		wg.Done()
-	}()
+// 	wg.Add(1)
+// 	go func() {
+// 		enqueueForQuotations(ctx)
+// 		wg.Done()
+// 	}()
 
-	wg.Add(1)
-	go func() {
-		enqueueForLanguages(ctx)
-		wg.Done()
-	}()
+// 	wg.Add(1)
+// 	go func() {
+// 		enqueueForLanguages(ctx)
+// 		wg.Done()
+// 	}()
 
-	wg.Wait()
+// 	wg.Wait()
 
-}
+// }
