8192d86340bdc0b25fa569a79ba9a312bb20c2c2
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-12-29 19:15:32 -0500

    Strip out whatlang

--- language-detector/statistics.go
+++ language-detector/statistics.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/abadojack/whatlanggo"
 	"github.com/jdkato/prose/v2"
 )
 
@@ -25,36 +24,34 @@ func tokenize(s string) (*prose.Document, error) {
 	return doc, nil
 }
 
-// CalculateLanguages computes the number of sentences identified as each language
-// using two different libraries. These are returned as maps of the languages and the
-// count of sentences.
-func CalculateLanguages(text string) (lingua LanguageStats, whatlang LanguageStats, err error) {
+// CalculateLanguages computes the number of sentences identified as each language.
+// This is returned as a map with a count of sentences matching each language.
+func CalculateLanguages(text string) (results LanguageStats, err error) {
 
 	// Keep track of the number of sentences with each language
-	lingua = make(LanguageStats)
-	whatlang = make(LanguageStats)
+	results = make(LanguageStats)
+	// whatlang = make(LanguageStats)
 
 	doc, err := tokenize(text)
 	if err != nil {
-		return nil, nil, fmt.Errorf("Error tokenizing text: %w", err)
+		return nil, fmt.Errorf("Error tokenizing text: %w", err)
 	}
 
 	// Count languages with both libraries
 	for _, s := range doc.Sentences() {
-		wl := whatlanggo.Detect(s.Text)
-		if wl.IsReliable() {
-			whatlang.incrementKey(wl.Lang.Iso6393())
-		} else {
-			whatlang.incrementKey("und")
-		}
+		// wl := whatlanggo.Detect(s.Text)
+		// if wl.IsReliable() {
+		// 	whatlang.incrementKey(wl.Lang.Iso6393())
+		// } else {
+		// 	whatlang.incrementKey("und")
+		// }
 
 		lang, exists := ldetector.DetectLanguageOf(s.Text)
 		if exists {
-			lingua.incrementKey(lang.IsoCode639_3().String())
+			results.incrementKey(lang.IsoCode639_3().String())
 		} else {
-			lingua.incrementKey("und")
+			results.incrementKey("UND")
 		}
-
 	}
 
 	return
