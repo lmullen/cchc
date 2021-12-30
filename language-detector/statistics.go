d112d890b0f0d5bfef4419c838c0aef9ef978e74
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-12-29 19:13:57 -0500

    Calculate Languages

--- language-detector/statistics.go
+++ language-detector/statistics.go
@@ -1,45 +1,60 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/abadojack/whatlanggo"
 	"github.com/jdkato/prose/v2"
 )
 
 type LanguageStats map[string]int
 
-func CalculateLanguages(s string) (lingua LanguageStats, whatlang LanguageStats, err error) {
+func (s LanguageStats) incrementKey(k string) {
+	s[k] += 1
+}
 
-	// Keep track of the number of sentences with each language
-	lingua = make(LanguageStats)
-	whatlang = make(LanguageStats)
-
+func tokenize(s string) (*prose.Document, error) {
 	doc, err := prose.NewDocument(s,
 		prose.WithExtraction(false),
 		prose.WithTagging(false),
 		prose.WithTokenization(false),
 		prose.WithSegmentation(true))
 	if err != nil {
-		return nil, nil, err
+		return nil, err
+	}
+	return doc, nil
+}
+
+// CalculateLanguages computes the number of sentences identified as each language
+// using two different libraries. These are returned as maps of the languages and the
+// count of sentences.
+func CalculateLanguages(text string) (lingua LanguageStats, whatlang LanguageStats, err error) {
+
+	// Keep track of the number of sentences with each language
+	lingua = make(LanguageStats)
+	whatlang = make(LanguageStats)
+
+	doc, err := tokenize(text)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error tokenizing text: %w", err)
 	}
 
-	// Count languages using whatlang
+	// Count languages with both libraries
 	for _, s := range doc.Sentences() {
 		wl := whatlanggo.Detect(s.Text)
 		if wl.IsReliable() {
-			whatlang[wl.Lang.String()] = whatlang[wl.Lang.String()] + 1
+			whatlang.incrementKey(wl.Lang.Iso6393())
 		} else {
-			whatlang["Unknown"] = whatlang["Unknown"] + 1
+			whatlang.incrementKey("und")
 		}
-	}
 
-	// Count languages using lingua
-	for _, s := range doc.Sentences() {
-		language, exists := ldetector.DetectLanguageOf(s.Text)
+		lang, exists := ldetector.DetectLanguageOf(s.Text)
 		if exists {
-			lingua[language.String()] = lingua[language.String()] + 1
+			lingua.incrementKey(lang.IsoCode639_3().String())
 		} else {
-			lingua["Unknown"] = lingua["Unknown"] + 1
+			lingua.incrementKey("und")
 		}
+
 	}
 
 	return
