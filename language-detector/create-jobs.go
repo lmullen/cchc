d3ead946cd0d03403faa6f9a1ce6e82afbca9721
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-12-30 21:28:24 -0500

    Demote a particular error

--- language-detector/create-jobs.go
+++ language-detector/create-jobs.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"strings"
 	"sync"
 	"time"
 
@@ -31,8 +32,22 @@ func createJobs(ctx context.Context, wg *sync.WaitGroup) {
 						log.Info("Checking again whether jobs need to be created")
 						continue
 					}
+
 				}
-				log.WithError(err).Error("Error creating job")
+				// Checking the error by value does not work
+				// if pgErr, ok := err.(*pgconn.PgError); !ok || pgErr.Code != "23505" {
+				// 	log.WithError(err).Debug("Attempt to create duplicate job failed")
+				// }
+				// So just check the bloody thing by matching strings
+				if strings.Contains(err.Error(), "SQLSTATE 23505") {
+					// TODO This is an error, but not at this problem of the program. So
+					// only log it if we really want to know all the dirty details. It doesn't
+					// actually cause a problem and needs to be fixed with the queuing locks
+					// in the jobs package.
+					log.WithError(err).Trace("Attempt to create duplicate job failed")
+				} else {
+					log.WithError(err).Error("Error creating job")
+				}
 				continue
 			}
 			log.WithField("job", job).Debug("Created job")
