f37424a7ed33d7c6fb0b2c9db4a3245a8e0860c4
Lincoln Mullen <lincoln@lincolnmullen.com>
2022-01-06 15:07:26 -0500

    Clean up cchc-ctrl options and documentation

--- cchc-ctrl/cmd/root.go
+++ cchc-ctrl/cmd/root.go
@@ -6,27 +6,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var force bool
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "cchc-ctrl",
 	Short: "Run database management commands",
-	Long: `This program runs configuration and management commands
-for your database. This includes migrations for the database
-as well as tasks such as removing skipped or failed jobs.
+	Long: `This program runs configuration and management commands for your database.
+This includes migrations for the database as well as tasks such as removing 
+skipped or failed jobs.
 
-To run a management task, use one of the sub commands. For example
-
-	cchc-ctrl ping
-
-will test whether you have a connection to the database.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+To run this program, you must set the CCHC_DBSTR environment variable to a 
+PostgreSQL connection string, such as the following:
+	postgres://user:password@hostname:5432/database?sslmode=disable
+`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -34,13 +33,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cchc-ctrl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().BoolVarP(&force, "force", "f", false, "run commands without waiting for user input")
 }
